ce91b5c9d4b3edd117eb19a1917f3a10de39fe86
Roaa-Ayman1 <103790328+Roaa-Ayman1@users.noreply.github.com>
2025-06-15 01:05:45 +0300

    Add files via upload

---
+++ Budget Tracker/src/environments/environment.default.ts
@@ -0,0 +1,12 @@
+
+export const defaultEnvironment = {
+  firebaseConfig: {
+    apiKey: "AIzaSyBsx5Uzm9qFFLAeRR7dR9nbgsR_qtPS1Vw",
+    authDomain: "budget-tracking-41264.firebaseapp.com",
+    projectId: "budget-tracking-41264",
+    storageBucket: "budget-tracking-41264.appspot.com",
+    messagingSenderId: "352114878100",
+    appId: "1:352114878100:web:fc521798c1a2493a697681",
+    measurementId: "G-NZ21LPLFKZ"
+  }
+};
